f5f16adbf233478f84172613c5ed3973684b6573
sara el koutbia <elkoutbs@tcd.ie>
2021-08-30 19:45:33 +0100

    Implementing the logic inside the PokerService

--- poker-game/PokerService.cs
+++ poker-game/PokerService.cs
@@ -23,6 +23,30 @@ namespace poker_game
         }
         internal void Run()
         {
+            _log.LogInformation(_deck.IsEmpty() ? "Deck is Empty." : "Deck is not empty.");
+            _deck.Initialize();
+
+            _deck.Pop();
+            _deck.Pop();
+
+            var numberOfCardsPerRound = _config.GetSection("PokerRules:#CardsToServeEachPlayer").Get<int>() * 2;//=10
+            while ((!_deck.IsEmpty()) && (_deck.Count >= numberOfCardsPerRound))
+            {
+                //deck is a stack - serve player 
+                _log.LogInformation("The deck has {count} cards. ", _deck.Count);
+
+                _deck.ServePlayer(_player);
+                _deck.ServePlayer(_computer);
+
+                //TODO: create players via factory 
+                Score playerScore = _player.EvaluateCards();
+                Score computerScore = _computer.EvaluateCards();
+
+                _log.LogInformation("Player score - Category:{p} Highest rank:{r}", playerScore._currentCategory, playerScore._currentHighestRank);
+                _log.LogInformation("Computer score - Category:{p} Highest rank:{r}", computerScore._currentHighestRank, computerScore._currentHighestRank);                
+                
+                //report the winner  
+            }
         }
 
     }
